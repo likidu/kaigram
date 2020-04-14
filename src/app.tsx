f0d703b8ad45f26526116cf01a052b8af9fe2301
Liya Du <liydu@microsoft.com>
2020-04-14 14:35:08 +0800

    Clean up

--- src/app.tsx
+++
@@ -1,8 +0,0 @@
-import { h, render } from 'preact';
-
-render((
-    <div id="foo">
-        <span>Hello, world again and again!</span>
-        <button onClick={ e => alert("hi!") }>Click Me</button>
-    </div>
-), document.body);
