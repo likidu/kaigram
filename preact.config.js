cd8422140399325c888c2d1cf900415a5b8e0f13
Liki Du <liya@liya.design>
2021-03-02 16:26:56 +0800

    Include tdweb folder

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,5 @@
+import { join } from 'path'
+
 export default {
     /**
      * Function that mutates the original webpack config.
@@ -9,6 +11,16 @@ export default {
      * @param {object} options - this is mainly relevant for plugins (will always be empty in the config), default to an empty object
      **/
     webpack(config, env, helpers, options) {
+        // Also including the tdweb folder for DevServer
+        if (!env.production) {
+            Object.assign(config.devServer, {
+                contentBase: [
+                    join(__dirname, '/src'),
+                    join(__dirname, '/tdweb/dist'),
+                ],
+            })
+        }
+
         // Remove default PostCSS options and use postcss.config.js instead
         const postCssLoaders = helpers.getLoadersByName(
             config,
