3eb5508f2bf5f1950c708065f530549a60492394
Liya Du <liydu@microsoft.com>
2022-08-19 18:29:13 -0700

    TDLib loaded okay

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,16 +1,17 @@
 const tailwindcss = require('tailwindcss');
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 const mode = process.env.NODE_ENV;
-const dev = mode === "development";
+const dev = mode === 'development';
 
 module.exports = {
-	plugins: [
-		tailwindcss,
-		autoprefixer,		
-		!dev && cssnano({
-			preset: "default",
-		}),
-	],
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    !dev &&
+      cssnano({
+        preset: 'default',
+      }),
+  ],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,32 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5000
+    port: 5000,
   },
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    // Copy tdweb files with dev server support
+    // https://github.com/sapphi-red/vite-plugin-static-copy
+    viteStaticCopy({
+      targets: [
+        {
+          src: ['tdweb/dist/**/*', '!**/tdweb.js'],
+          dest: '.',
+        },
+      ],
+    }),
+  ],
+  assetsInclude: ['**/*.worker.js, **/*.wasm'],
+  optimizeDeps: {
+    include: ['tdweb'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/tdweb/, /node_modules/],
+    },
+  },
 });
